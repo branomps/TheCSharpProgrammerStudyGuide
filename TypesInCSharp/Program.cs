a04abbea63b987c7ae2fae4b1956bd14a0707006
Branislav Macosinec <branislav.macosinec@windowslive.com>
2018-11-24 13:15:06 +0000

    Enum enumeration is a set of named Constant.

--- TypesInCSharp/Program.cs
+++ TypesInCSharp/Program.cs
@@ -10,6 +10,32 @@ namespace TypesInCSharp
     {
         static void Main(string[] args)
         {
+            Status player = Status.Alive;
+            switch (player)
+            {
+                case Status.Alive:
+                    Console.WriteLine($"Player is {player}");
+                    break;
+                case Status.Injured:
+                    Console.WriteLine($"Player is {player}");
+                    break;
+                case Status.Dead:
+                    Console.WriteLine($"Player is {player}");
+                    break;
+                default:
+                    Console.WriteLine("Player is retired.  :-D");
+                    break;
+            }
+            Console.ReadLine();
         }
+
+        
+    }
+
+    enum Status
+    {
+        Alive,
+        Injured,
+        Dead
     }
 }
